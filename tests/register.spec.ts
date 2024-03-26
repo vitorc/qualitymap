734cedee1a2eb41cc810cbe9def654ba507f096f
Vitor Cardoso dos Santos <VSantos@getcaret.com>
2024-03-26 18:10:54 -0300

    Create test cases to test a success register and a register with error

---
+++ tests/register.spec.ts
@@ -0,0 +1,40 @@
+import { chromium, expect, test } from '@playwright/test';
+import { RegisterUser } from '../pageObjects/register.page';
+
+test.describe('User registration', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/register");
+        await expect(page).toHaveTitle("nopCommerce demo store. Register");
+
+    })
+    test.afterAll(async()=>{
+        const browser = await chromium.launch();
+        await browser.close();
+    })
+    
+    test('Successful user registration', async ({ page }) => {
+        const registerUser = new RegisterUser(page);
+        await registerUser.fillRegistrationForm('FirstNameUser', 'LastNameUser', 'test@test3.com', 'Company Test', 'test123', 'test123');
+        await registerUser.selectDateOfBirth(28);
+        await registerUser.selectMonthOfBirth(11);
+        await registerUser.selectYearOfBirth("1985");
+        await registerUser.uncheckNewsletter();
+        await registerUser.clickRegisterButton();
+        await registerUser.verifyRegistrationSuccess();
+    })
+
+    
+    test('Registration email already exists', async ({ page }) => {
+        const registerUser = new RegisterUser(page);
+        await registerUser.fillRegistrationForm('FirstNameUser', 'LastNameUser', 'test@test33.com', 'Company Test', 'test123', 'test123');
+        await registerUser.selectDateOfBirth(28);
+        await registerUser.selectMonthOfBirth(11);
+        await registerUser.selectYearOfBirth("1985");
+        await registerUser.uncheckNewsletter();
+        await registerUser.clickRegisterButton();
+        await registerUser.verifyEmailAlreadyExists();
+    })
+
+})
+
+
